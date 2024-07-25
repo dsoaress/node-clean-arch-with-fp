8f25930689167e0ea5a009de1d56f0742a3465bd
Daniel Soares <daniel.soares@me.com>
2024-07-25 03:19:10 -0300

    feat: add sonar (#3)

--- src/infra/server.ts
+++ src/infra/server.ts
@@ -6,6 +6,7 @@ import { inputBodyParserErrorHandler } from "./http/middleware/input-body-parser
 export async function server(): Promise<void> {
   const app = express();
   app.use(express.json());
+  app.disable("x-powered-by");
   app.use(inputBodyParserErrorHandler);
   app.use(setupRouter());
   app.listen(3000, () => console.log("Server is running on port 3000"));
